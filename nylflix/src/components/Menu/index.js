1cbaa7760b121208db8d50f9d34d0294c7182b09
dvsilva <dvsilva@gmail.com>
2020-07-28 10:24:37 -0300

    aula 2 + desafio flappy bird

--- nylflix/src/components/Menu/index.js
+++ nylflix/src/components/Menu/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
-import './Menu.css';
+import './menu.css';
 import Button from '../Button';
 // import ButtonLink from './components/ButtonLink';
 
